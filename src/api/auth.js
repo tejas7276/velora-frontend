fdcdc1e1246048ce0384f5d4450ea464af07b0b0
Tejas-Shinde <tejasshinde7276@gmail.com>
2026-04-05 19:06:12 +0530

    fix axios crash

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,4 @@
 import api from './axios'
 
-const API = axios.create({
-  baseURL: "https://velora-backend-7qjl.onrender.com/api",
-});
-
 export const login = (data) => api.post('/auth/login', data)
 export const register = (data) => api.post('/auth/register', data)
